458d7606c712b24807fb734b0c524ec8dc11b3a4
devvindan <devvindan@gmail.com>
2018-11-08 19:56:52 +0300

    finished with result structures

---
+++ AssemblyBrowserLibrary/ResultStructure/Namespace.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AssemblyBrowserLibrary.ResultStructure
+{
+    // result structure for namespace
+    public class Namespace
+    {
+        public string Name { set; get; }
+
+        public List<Datatype> DataTypes { set; get; }
+
+        public Namespace(string name)
+        {
+            Name = name;
+            DataTypes = new List<Datatype>();
+        }
+    }
+}
---
+++ AssemblyBrowserLibrary/ResultStructure/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowserLibrary.ResultStructure
+{
+    // structure for result representation
+    public class Result
+    {
+        public List<Namespace> Namespaces { set; get; }
+
+        public Result()
+        {
+            Namespaces = new List<Namespace>();
+        }
+    }
+}
