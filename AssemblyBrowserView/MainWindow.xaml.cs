534b3177cf75322b2a70e589e6009dad013442a4
devvindan <devvindan@gmail.com>
2018-11-08 21:13:15 +0300

    added view

--- AssemblyBrowserView/MainWindow.xaml.cs
+++ AssemblyBrowserView/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,6 +22,7 @@ namespace AssemblyBrowserView
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new AssemblyBrowserLibrary.ViewModel();
         }
     }
 }
