f7d22dd12b5813d118bdb0fed01982fc53bf652f
devvindan <devvindan@gmail.com>
2018-11-08 19:35:15 +0300

    created filed structure

---
+++ AssemblyBrowserLibrary/ResultStructure/Filed.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+
+namespace AssemblyBrowserLibrary.ResultStructure
+{
+    // Result structure for "field"
+    public class Field
+    {
+        public string name;
+        public string type;
+
+        // recursive generic type builder
+        private string GetGenericType(Type[] t)
+        {
+            string result = "";
+            foreach (Type genericType in t)
+            {
+                if (t.GetType().IsGenericType)
+                    result += "<" + GetGenericType(genericType.GetType().GenericTypeArguments) + ">";
+                else
+                    result += genericType.Name + " ";
+            }
+
+            return result;
+        }
+
+        public Field(FieldInfo fieldInfo)
+        {
+            name = fieldInfo.Name;
+            type = TypeInfoDecoder.GetTypeModifiers(fieldInfo.GetType());
+
+            // in case the type is Generic<T>
+            if (fieldInfo.FieldType.IsGenericType)
+                type += fieldInfo.FieldType.Name + "<" +
+                    GetGenericType(fieldInfo.FieldType.GenericTypeArguments) + ">";
+            else
+                type += fieldInfo.FieldType.Name;
+        }
+
+    }
+}
---
+++ AssemblyBrowserLibrary/TypeInfoDecoder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowserLibrary
+{
+    public static class TypeInfoDecoder
+    {
+
+        // get string version of type modifiers (public, private, etc..)
+        public static string GetTypeModifiers(Type typeInfo)
+        {
+            if (typeInfo.IsNestedPrivate) return "private ";
+            if (typeInfo.IsNestedFamily) return "protected ";
+            if (typeInfo.IsNestedAssembly) return "internal ";
+            if (typeInfo.IsNestedFamORAssem) return "protected internal ";
+            if (typeInfo.IsNestedFamANDAssem) return "protected private ";
+            if (typeInfo.IsNestedPublic || typeInfo.IsPublic) return "public ";
+
+            if (typeInfo.IsNotPublic)
+                return "private ";
+            else
+                return "public ";
+        }
+
+        // string version of attributes (static, sealed, etc..)
+        private static string GetTypeAtributes(Type typeInfo)
+        {
+            if (typeInfo.IsAbstract && typeInfo.IsSealed) return "static ";
+            if (typeInfo.IsSealed) return "sealed ";
+            if (typeInfo.IsAbstract) return "abstract ";
+
+            return " ";
+
+        }
+
+        // string version of the class (inteface, struct, etc..)
+        private static string GetTypeClass(Type typeInfo)
+        {
+            if (typeInfo.IsInterface) return "interface ";
+            if (typeInfo.IsValueType) return "struct ";
+            if (typeInfo.IsEnum) return "enum ";
+            if (typeInfo.BaseType == typeof(MulticastDelegate)) return "delegate ";
+            if (typeInfo.IsClass) return "class ";
+
+            return "";
+
+        }
+
+        // get all information 
+        public static string GetAtributes(Type typeInfo)
+        {
+            return GetTypeModifiers(typeInfo) + GetTypeAtributes(typeInfo) + GetTypeClass(typeInfo);
+        }
+
+    }
+}
