be19f80b512637cba6a93e28e3e1dec30c48a7a1
devvindan <devvindan@gmail.com>
2018-11-08 19:42:41 +0300

    added property structure

---
+++ AssemblyBrowserLibrary/ResultStructure/Property.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace AssemblyBrowserLibrary.ResultStructure
+{
+    public class Property
+    {
+        public string name;
+        public string type;
+
+        public Property(PropertyInfo propertyInfo)
+        {
+            name = propertyInfo.Name;
+            type = propertyInfo.PropertyType.Name;
+        }
+    }
+}
