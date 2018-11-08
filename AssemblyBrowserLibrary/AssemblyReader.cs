5b8ba15ff48b0bfda1f5664547df5eab7680ea30
devvindan <devvindan@gmail.com>
2018-11-08 20:38:31 +0300

    viewmodel added

---
+++ AssemblyBrowserLibrary/AssemblyReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AssemblyBrowserLibrary.ResultStructure;
+using System.Reflection;
+
+namespace AssemblyBrowserLibrary
+{
+    public class AssemblyReader
+    {
+        private Result _result;
+
+        public AssemblyReader()
+        {
+
+        }
+
+        // read assembly info to result datasctructure
+        public Result Read(string path)
+        {
+            _result = new Result();
+
+            Assembly asm = Assembly.LoadFrom(path);
+
+            //working with namespaces
+            foreach (var type in asm.DefinedTypes)
+            {
+                // prevent from adding namespaces already in the result list, add namespaces
+                if (_result.Namespaces.Find(x => x.Name == type.Namespace) == null
+                    && type.Namespace != null)
+                {
+                    _result.Namespaces.Add(new Namespace(type.Namespace));
+                }
+            }
+
+            //working with dataTypes in each namespace
+            foreach (var ns in _result.Namespaces)
+            {
+                // for each namespace add corresponding datatype info
+                foreach (var type in asm.DefinedTypes.Where(x => x.Namespace == ns.Name))
+                {
+                    ns.DataTypes.Add(new Datatype(type));
+                }
+            }
+
+            return _result;
+        }
+    }
+}
---
+++ AssemblyBrowserLibrary/ViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using AssemblyBrowserLibrary.ResultStructure;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace AssemblyBrowserLibrary
+{
+    // ViewModel part in MVVM
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Result _result;
+        private AssemblyReader _asmReader;
+        private Command _openCommand;
+
+        public ViewModel()
+        {
+            _asmReader = new AssemblyReader();
+        }
+
+        public Result Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+
+                // if result is changed, 
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // if even is not null, invoke it
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Command OpenCommand
+        {
+            get
+            {
+                // if command is not created, create one
+                return _openCommand ??
+                    (_openCommand = new Command(obj =>
+                    {
+                        try
+                        {
+                            // reading selected assembly
+                            OpenFileDialog openFileDialog = new OpenFileDialog();
+                            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                            {
+                                Result = _asmReader.Read(openFileDialog.FileName);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.ToString());
+                        }
+                    }));
+            }
+        }
+    }
+}
