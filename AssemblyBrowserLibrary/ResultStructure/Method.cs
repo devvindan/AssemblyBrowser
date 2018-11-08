e9062656e98ebb9e9a898da3c585c076d1966af0
devvindan <devvindan@gmail.com>
2018-11-08 19:39:30 +0300

    added method structure

---
+++ AssemblyBrowserLibrary/ResultStructure/Method.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+
+namespace AssemblyBrowserLibrary.ResultStructure
+{
+    class Method
+    {
+        public string name;
+        public string signature;
+
+        public Method(MethodInfo methodInfo)
+        {
+            signature = TypeInfoDecoder.GetTypeModifiers(methodInfo.GetType()) + methodInfo.ToString();
+        }
+
+    }
+}
